d5bf5cca593551def8e50570d79cb13e0c8818b7
vasilistheiou <vtheiou@outlook.com.gr>
2025-05-12 02:50:05 +0300

    Fixed CV button

--- front-end/src/components/HomePage/HomePage.js
+++ front-end/src/components/HomePage/HomePage.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './HomePage.css';
 import { Link } from 'react-router-dom';
 import myImage from "../../Images/Me/Myself.jpg"
-
+import CV from "./MyCV.pdf"
 
 const HomePage = () => {
+
+
   return (
     <div className="homepage">
       <div className="intro">
@@ -14,9 +16,10 @@ const HomePage = () => {
         <Link to="/contact" className="contact-link">
           <button className="contact-button">Contact</button>
         </Link>
-          <a href="/MyCV.pdf" download>
-            <button className="contact-button">CV</button>
-          </a>
+        <a href={CV} download="Theiou_CV.pdf" >
+          <button className="contact-button" >CV</button>
+        </a>
+
       </div>
       <div className="photo">
         <img src={myImage} alt="Vasilis" />
